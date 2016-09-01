e5dbea82247e82cb18e7dc510355a6cbb438e1ca
drbarrett <barrettd@cs.cmu.edu>
2016-09-01 16:08:21 -0400

    General code improvements.

--- epan/dissectors/packet-xia-xcmp.c
+++ epan/dissectors/packet-xia-xcmp.c
@@ -32,7 +32,9 @@
 
 #include "config.h"
 #include <epan/packet.h>
+#include <epan/expert.h>
 #include <epan/capture_dissectors.h>
+#include "packet-xip.h"
 
 static int proto_xcmp = -1;
 static int hf_xcmp_type = -1;
@@ -44,13 +46,14 @@ static int hf_xcmp_chksum = -1;
 static int hf_xcmp_time = -1;
 static int hf_xcmp_time_relative = -1;
 
+static expert_field ei_xcmp_invalid_len = EI_INIT;
+
 static gint ett_xcmp = -1;
 
 static dissector_handle_t xcmp_handle;
+static dissector_handle_t xip_handle;
 
-#define XIA_NEXT_HEADER_XCMP 0x01
-
-/*  Offsets of fields within an XCMP packet */
+/* Offsets of fields within an XCMP packet */
 #define XCMP_TYPE   0
 #define XCMP_CODE   1
 #define XCMP_CHKSUM 2
@@ -60,22 +63,17 @@ static dissector_handle_t xcmp_handle;
 #define XCMP_ECHO_SEQ  6
 #define XCMP_ECHO_TIME 8
 
+/* minimum packet sizes */
 #define MIN_XCMP_HEADER_SIZE 4
 #define MIN_ECHO_HEADER_SIZE 8
 #define NUM_TIME_BYTES       8
 
 /* XIA only uses a limited # of types */
-#define    XCMP_ECHOREPLY 0
-#define    XCMP_UNREACH   3
-#define    XCMP_REDIRECT  5
-#define    XCMP_ECHO      8
-#define    XCMP_TIMXCEED  11
-
-/* XCMP unreachable codes */
-#define XCMP_UNREACH_NET         0
-#define XCMP_UNREACH_HOST        1
-#define XCMP_UNREACH_INTENT      3
-#define XCMP_UNREACH_UNSPECIFIED 16
+#define XCMP_ECHOREPLY 0
+#define XCMP_UNREACH   3
+#define XCMP_REDIRECT  5
+#define XCMP_ECHO      8
+#define XCMP_TIMXCEED  11
 
 static const value_string type_vals[] = {
     { XCMP_ECHOREPLY, "Pong"        },
@@ -85,43 +83,18 @@ static const value_string type_vals[] = {
     { XCMP_TIMXCEED,  "TTL Exceeded"},
     {0, NULL}};
 
+/* XCMP unreachable codes */
+#define XCMP_UNREACH_NET         0
+#define XCMP_UNREACH_HOST        1
+#define XCMP_UNREACH_INTENT      3
+#define XCMP_UNREACH_UNSPECIFIED 16
+
 static const value_string unreach_vals[] = {
     { XCMP_UNREACH_NET,         "Network" },
     { XCMP_UNREACH_HOST,        "Host"    },
     { XCMP_UNREACH_INTENT,      "Intent"  },
     { XCMP_UNREACH_UNSPECIFIED, "Unknown" },
     {0, NULL}};
-
-/*  FIXME: move this stuff to a common file */
-/*  XIA principals */
-#define XIDTYPE_NAT    0x00
-#define XIDTYPE_AD     0x10
-#define XIDTYPE_HID    0x11
-#define XIDTYPE_CID    0x12
-#define XIDTYPE_SID    0x13
-#define XIDTYPE_UNI4ID 0x14
-#define XIDTYPE_I4ID   0x15
-#define XIDTYPE_U4ID   0x16
-#define XIDTYPE_XDP    0x17
-#define XIDTYPE_SRVCID 0x18
-#define XIDTYPE_FLOWID 0x19
-#define XIDTYPE_ZF     0x20
-
-/*  Principal string values */
-static const value_string xidtype_vals[] = {
-    { XIDTYPE_AD,     "ad" },
-    { XIDTYPE_HID,    "hid" },
-    { XIDTYPE_CID,    "cid" },
-    { XIDTYPE_SID,    "sid" },
-    { XIDTYPE_UNI4ID, "uni4id" },
-    { XIDTYPE_I4ID,   "i4id" },
-    { XIDTYPE_U4ID,   "u4id" },
-    { XIDTYPE_XDP,    "xdp" },
-    { XIDTYPE_SRVCID, "serval" },
-    { XIDTYPE_FLOWID, "flowid" },
-    { XIDTYPE_ZF,     "zf" },
-    { 0,              NULL }
-};
 
 gboolean
 capture_xcmp(const guchar *pd _U_, int offset _U_, int len _U_, capture_packet_info_t *cpinfo, const union wtap_pseudo_header *pseudo_header _U_)
@@ -133,54 +106,68 @@ capture_xcmp(const guchar *pd _U_, int offset _U_, int len _U_, capture_packet_i
 static int
 dissect_xcmp(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void* data _U_)
 {
-    gint8       type;
-    gint8       code;
-    guint16     chksum;
-    guint16     seq;
-    guint16     id;
-    guint16     tot_len;
-    guint16     actual_len;
-    proto_tree *xcmp_tree = NULL;
+    guint8  type, code;
+    guint16 chksum;
+    guint16 seq, id;
+    guint16 header_len, actual_len;
+    tvbuff_t *next_tvb, *xip_tvb;
+    nstime_t ts, time_relative;
     proto_item *ti;
-    tvbuff_t   *next_tvb;
-    nstime_t    ts, time_relative;
+    proto_tree *xcmp_tree = NULL;
+
+    /* make sure we have enough data for a simple XCMP header */
+    actual_len = tvb_reported_length(tvb);
+    if (actual_len < MIN_XCMP_HEADER_SIZE) {
+        return 0;
+    }
 
     col_set_str(pinfo->cinfo, COL_PROTOCOL, "XCMP");
-
     type = tvb_get_guint8(tvb, XCMP_TYPE);
     code = tvb_get_guint8(tvb, XCMP_CODE);
     chksum = tvb_get_ntohs(tvb, XCMP_CHKSUM);
 
-    actual_len = tvb_reported_length(tvb);
+    /* compute header size required */
     if (type == XCMP_ECHO || type == XCMP_ECHOREPLY) {
-
-        tot_len = MIN_ECHO_HEADER_SIZE + NUM_TIME_BYTES;
-        if (tot_len > actual_len) {
-            tot_len = MIN_ECHO_HEADER_SIZE;
+        header_len = MIN_ECHO_HEADER_SIZE + NUM_TIME_BYTES;
+        if (header_len > actual_len) {
+            header_len = MIN_ECHO_HEADER_SIZE;
         }
     } else {
-        tot_len = MIN_XCMP_HEADER_SIZE;
+        header_len = MIN_XCMP_HEADER_SIZE;
     }
 
-    if (tree) {
-        ti = proto_tree_add_protocol_format(tree, proto_xcmp, tvb, 0, tot_len, "XIA Control Message Protocol");
-        xcmp_tree = proto_item_add_subtree(ti, ett_xcmp);
-        proto_tree_add_item(xcmp_tree, hf_xcmp_type, tvb, XCMP_TYPE, 1, ENC_NA);
-        if (type != XCMP_UNREACH) {
-            proto_tree_add_item(xcmp_tree, hf_xcmp_code, tvb, XCMP_CODE, 1, ENC_NA);
-        } else {
-            proto_tree_add_item(xcmp_tree, hf_xcmp_unreach_code, tvb, XCMP_CODE, 1, ENC_NA);
-        }
-        proto_tree_add_uint(xcmp_tree, hf_xcmp_chksum, tvb, XCMP_CHKSUM, 2, chksum);
+    /* display common fields */
+    ti = proto_tree_add_protocol_format(tree, proto_xcmp, tvb, 0, header_len, "XIA Control Message Protocol");
+    xcmp_tree = proto_item_add_subtree(ti, ett_xcmp);
+    proto_tree_add_item(xcmp_tree, hf_xcmp_type, tvb, XCMP_TYPE, 1, ENC_NA);
+    if (type != XCMP_UNREACH) {
+        proto_tree_add_item(xcmp_tree, hf_xcmp_code, tvb, XCMP_CODE, 1, ENC_NA);
+    } else {
+        proto_tree_add_item(xcmp_tree, hf_xcmp_unreach_code, tvb, XCMP_CODE, 1, ENC_NA);
+    }
+    proto_tree_add_uint(xcmp_tree, hf_xcmp_chksum, tvb, XCMP_CHKSUM, 2, chksum);
+
+    if (header_len > actual_len) {
+        // not enough room for the extended header information
+        expert_add_info_format(pinfo, ti, &ei_xcmp_invalid_len,
+            "Packet size is too small: received %d bytes, need %d",
+            actual_len, MIN_ECHO_HEADER_SIZE);
+            return MIN_XCMP_HEADER_SIZE;
     }
 
     switch (type) {
     case XCMP_UNREACH:
-        col_add_fstr(pinfo->cinfo, COL_INFO, "%s Foo is unreachable", try_val_to_str(code, unreach_vals));
+        xip_tvb = tvb_new_subset_remaining(tvb, 8);
+        call_dissector(xip_handle, xip_tvb, pinfo, tree);
+        col_add_fstr(pinfo->cinfo, COL_INFO, "%s is unreachable", try_val_to_str(code, unreach_vals));
+        return tvb_captured_length(tvb);
         break;
 
     case XCMP_TIMXCEED:
+        xip_tvb = tvb_new_subset_remaining(tvb, 8);
+        call_dissector(xip_handle, xip_tvb, pinfo, tree);
         col_add_fstr(pinfo->cinfo, COL_INFO, "TTL Exceeded");
+        return tvb_captured_length(tvb);
         break;
 
     case XCMP_ECHOREPLY:
@@ -190,32 +177,27 @@ dissect_xcmp(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void* data _U_
         proto_tree_add_uint(xcmp_tree, hf_xcmp_id,  tvb, XCMP_ECHO_ID,  2, id);
         proto_tree_add_uint(xcmp_tree, hf_xcmp_seq, tvb, XCMP_ECHO_SEQ, 2, seq);
 
-        if (tot_len >= MIN_ECHO_HEADER_SIZE + NUM_TIME_BYTES) {
+        if (header_len >= MIN_ECHO_HEADER_SIZE + NUM_TIME_BYTES) {
             ts.secs = tvb_get_ntohl(tvb, XCMP_ECHO_TIME);
             ts.nsecs = tvb_get_ntohl(tvb, XCMP_ECHO_TIME + 4);    /* Leave at microsec resolution for now */
-            if ((guint32) (ts.secs - pinfo->abs_ts.secs) >=
-                3600 * 24 || ts.nsecs >= 1000000) {
+
+            if ((guint32) (ts.secs - pinfo->abs_ts.secs) >= 3600 * 24 || ts.nsecs >= 1000000) {
                 /* Timestamp does not look right in BE, try LE representation */
                 ts.secs = tvb_get_letohl(tvb, 8);
-                ts.nsecs = tvb_get_letohl(tvb, 8 + 4);    /* Leave at microsec resolution for now */
+                ts.nsecs = tvb_get_letohl(tvb, 8 + 4);            /* Leave at microsec resolution for now */
             }
-            if ((guint32) (ts.secs - pinfo->abs_ts.secs) <
-                3600 * 24 && ts.nsecs < 1000000) {
-                ts.nsecs *= 1000;    /* Convert to nanosec resolution */
-                proto_tree_add_time(xcmp_tree, hf_xcmp_time,
-                            tvb, XCMP_ECHO_TIME, 8, &ts);
-                nstime_delta(&time_relative, &pinfo->abs_ts,
-                         &ts);
-                ti = proto_tree_add_time(xcmp_tree,
-                             hf_xcmp_time_relative,
-                             tvb, 8, 8,
-                             &time_relative);
+            if ((guint32) (ts.secs - pinfo->abs_ts.secs) < 3600 * 24 && ts.nsecs < 1000000) {
+                /* Convert to nanosec resolution */
+                ts.nsecs *= 1000;
+                proto_tree_add_time(xcmp_tree, hf_xcmp_time, tvb, XCMP_ECHO_TIME, 8, &ts);
+                nstime_delta(&time_relative, &pinfo->abs_ts, &ts);
+                ti = proto_tree_add_time(xcmp_tree, hf_xcmp_time_relative, tvb, 8, 8, &time_relative);
                 PROTO_ITEM_SET_GENERATED(ti);
-           }
+            }
         }
+
         col_add_fstr(pinfo->cinfo, COL_INFO, (type == XCMP_ECHO) ? "Ping" : "Pong");
-        col_append_fstr(pinfo->cinfo, COL_INFO, " id=0x%04x, seq=%u",
-            tvb_get_ntohs(tvb, 4), tvb_get_ntohs(tvb, 6));
+        col_append_fstr(pinfo->cinfo, COL_INFO, " id=0x%04x, seq=%u", tvb_get_ntohs(tvb, 4), tvb_get_ntohs(tvb, 6));
         break;
 
     default:
@@ -224,7 +206,7 @@ dissect_xcmp(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void* data _U_
     }
 
     /* everything else is data */
-    next_tvb = tvb_new_subset_remaining(tvb, tot_len);
+    next_tvb = tvb_new_subset_remaining(tvb, header_len);
     call_data_dissector(next_tvb, pinfo, tree);
 
     return tvb_captured_length(tvb);
@@ -234,27 +216,41 @@ void
 proto_register_xcmp(void)
 {
     static hf_register_info hf[] = {
-        { &hf_xcmp_type,   { "Type", "xcmp.type",         FT_UINT8,  BASE_DEC, VALS(type_vals), 0x0, NULL, HFILL }},
-        { &hf_xcmp_code,   { "Code", "xcmp.code",         FT_UINT8,  BASE_DEC, NULL, 0x0, NULL, HFILL }},
-        { &hf_xcmp_unreach_code,   { "Code", "xcmp.unreach_code", FT_UINT8,  BASE_DEC, VALS(unreach_vals), 0x0, NULL, HFILL }},
-        { &hf_xcmp_chksum, { "Checksum", "xcmp.chksum",   FT_UINT16, BASE_HEX, NULL, 0x0, NULL, HFILL }},
-        { &hf_xcmp_id,     { "ID", "xcmp.echo.id",        FT_UINT16, BASE_HEX, NULL, 0x0, NULL, HFILL }},
-        { &hf_xcmp_seq,    { "Sequence No", "xcmp.echo.seq", FT_UINT16, BASE_DEC, NULL, 0x0, NULL, HFILL }},
+        { &hf_xcmp_type, { "Type", "xcmp.type",
+            FT_UINT8,  BASE_DEC, VALS(type_vals), 0x0, NULL, HFILL }},
 
-        {&hf_xcmp_time,
-         {"Timestamp from xcmp data", "xcmp.echo.time",
-          FT_ABSOLUTE_TIME,
-          ABSOLUTE_TIME_LOCAL, NULL, 0x0,
-          "The timestamp in the first 8 bytes of the xcmp data",
-          HFILL}},
+        { &hf_xcmp_code, { "Code", "xcmp.code",
+            FT_UINT8,  BASE_DEC, NULL, 0x0, NULL, HFILL }},
 
-        {&hf_xcmp_time_relative,
-         {"Timestamp from xcmp data (relative)",
-          "xcmp.echo.time_relative",
-          FT_RELATIVE_TIME, BASE_NONE, NULL, 0x0,
-          "The timestamp of the packet, relative to the timestamp in the first 8 bytes of the xcmp data",
-          HFILL}}
+        { &hf_xcmp_unreach_code, { "Code", "xcmp.unreach_code",
+            FT_UINT8,  BASE_DEC, VALS(unreach_vals), 0x0, NULL, HFILL }},
+
+        { &hf_xcmp_chksum, { "Checksum", "xcmp.chksum",
+            FT_UINT16, BASE_HEX, NULL, 0x0, NULL, HFILL }},
+
+        { &hf_xcmp_id, { "ID", "xcmp.echo.id",
+            FT_UINT16, BASE_HEX, NULL, 0x0, NULL, HFILL }},
+
+        { &hf_xcmp_seq, { "Sequence No", "xcmp.echo.seq",
+            FT_UINT16, BASE_DEC, NULL, 0x0, NULL, HFILL }},
+
+        { &hf_xcmp_time, { "Timestamp from xcmp data", "xcmp.echo.time",
+            FT_ABSOLUTE_TIME, ABSOLUTE_TIME_LOCAL, NULL, 0x0,
+            "The timestamp in the first 8 bytes of the xcmp data",
+            HFILL}},
+
+        {&hf_xcmp_time_relative, {"Timestamp from xcmp data (relative)", "xcmp.echo.time_relative",
+            FT_RELATIVE_TIME, BASE_NONE, NULL, 0x0,
+            "The timestamp of the packet, relative to the timestamp in the first 8 bytes of the xcmp data",
+            HFILL}}
     };
+
+    static ei_register_info ei[] = {
+        { &ei_xcmp_invalid_len, { "xcmp.invalid.len",
+            PI_MALFORMED, PI_ERROR, "Invalid length", EXPFILL }},
+    };
+
+    expert_module_t* expert_xcmp;
 
     static gint *ett[] = {
         &ett_xcmp,
@@ -265,6 +261,9 @@ proto_register_xcmp(void)
     proto_register_field_array(proto_xcmp, hf, array_length(hf));
     proto_register_subtree_array(ett, array_length(ett));
 
+    expert_xcmp = expert_register_protocol(proto_xcmp);
+    expert_register_field_array(expert_xcmp, ei, array_length(ei));
+
     xcmp_handle = register_dissector( "xcmp" , dissect_xcmp, proto_xcmp );
 }
 
@@ -273,6 +272,8 @@ proto_reg_handoff_xcmp(void)
 {
     xcmp_handle = create_dissector_handle(dissect_xcmp, proto_xcmp);
     dissector_add_uint("xip.next_hdr", XIA_NEXT_HEADER_XCMP, xcmp_handle);
+
+    xip_handle = find_dissector("xip");
 }
 
 /*
